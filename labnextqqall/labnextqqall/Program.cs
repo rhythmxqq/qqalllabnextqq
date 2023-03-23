6ea954a2f6477c19b3acb65f5234dc85bb02fd56
rhythmxqq <ktlall@mail.ru>
2023-03-23 10:43:37 +0300

    123
    123

---
+++ labnextqqall/labnextqqall/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    delegate string GetNextDayOfWeek();
+
+    static void Main(string[] args)
+    {
+        string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        int currentDayIndex = 0;
+
+        GetNextDayOfWeek getNextDay = () =>
+        {
+            currentDayIndex = (currentDayIndex + 1) % 7;
+            return daysOfWeek[currentDayIndex];
+        };
+
+        // Вызываем делегат 10 раз
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(getNextDay());
+        }
+    }
+}
